1df8df555ffc426ba4f762141bbc370e58a2380f
William Pinto <williamporto_14@hotmail.com>
2022-03-02 09:43:14 -0300

    Adding more behaviour to form

--- src/components/NewExpenseForm/NewExpenseForm.jsx
+++ src/components/NewExpenseForm/NewExpenseForm.jsx
@@ -10,6 +10,7 @@ const initializedForm = () => {
 
 const NewExpenseForm = ({ onAddExpense }) => {
   const [formData, setFormData] = useState(initializedForm);
+  const [formClosed, setFormClosed] = useState(true);
 
   const titleChangeHandler = (event) => {
     // This way react guarantees that the state it will use to be updated will be the latest one (in the previous way, since react updates the states asynchronously,
@@ -48,6 +49,11 @@ const NewExpenseForm = ({ onAddExpense }) => {
     });
   };
 
+  const resetForm = () => {
+    setFormClosed(true);
+    setFormData(initializedForm);
+  };
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
     const newData = {
@@ -56,46 +62,61 @@ const NewExpenseForm = ({ onAddExpense }) => {
       date: new Date(formData.date),
     };
 
-    setFormData(initializedForm);
+    resetForm();
     onAddExpense(newData);
+  };
+
+  const newExpenseHandler = (event) => {
+    event.preventDefault();
+    setFormClosed(false);
+  };
+
+  const cancelNewExpenseHandler = (event) => {
+    event.preventDefault();
+    resetForm();
   };
 
   return (
     <section className="new-expense">
-      <form className="new-expense-controls" onSubmit={onSubmitHandler}>
-        <div className="new-expense-control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={formData.title}
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className="new-expense-control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={formData.amount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense-control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2018-01-01"
-            max="2023-12-31"
-            value={formData.date}
-            onChange={dateChangeHandler}
-            className={formData.date ? "" : "empty-date"}
-          />
-        </div>
-        <div className="new-expense-action">
-          <button type="submit">Add</button>
-        </div>
-      </form>
+      {formClosed ? (
+        <button onClick={newExpenseHandler}>Add new expense</button>
+      ) : (
+        <form className="new-expense-controls" onSubmit={onSubmitHandler}>
+          <div className="new-expense-control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={formData.title}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className="new-expense-control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={formData.amount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-expense-control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2018-01-01"
+              max="2023-12-31"
+              value={formData.date}
+              onChange={dateChangeHandler}
+              className={formData.date ? "" : "empty-date"}
+            />
+          </div>
+          <div className="new-expense-action">
+            <button onClick={cancelNewExpenseHandler}>Cancel</button>
+            <button type="submit">Add</button>
+          </div>
+        </form>
+      )}
     </section>
   );
 };
